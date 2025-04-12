f38b2f7ba4e25423915109634c43b149630f1fcc
SaiabhishekCH <saiabishek305@gmail.com>
2025-04-12 20:51:52 +0530

    Feat : Loading animation

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,25 +3,32 @@ import { Button } from "../components/button";
 import { Input } from "../components/input";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import { Loading } from "../icons.tsx/loading";
 
 export function HomePage(props : any){
     const navigate = useNavigate();
     const[ allUsers , setAllUsers ] = useState([]);
     const [ userName , setUserName ] = useState("");
     const [ balance , setBalance ] = useState("");
+    const [ isLoading , setIsLoading ] = useState(true);
+
 
     useEffect(()=>{
         async function getUser(){
+            setIsLoading(true);
             const response = await axios.get("http://localhost:8080/user/getUser",{
-
                 headers : {
                     authorization : localStorage.getItem("token")
                 }
             })
+            const timout = setTimeout(()=>{
+                setIsLoading(false);
+            },1500);
             setAllUsers(response.data.user);
+            return () => clearTimeout(timout); 
         }
         getUser();
-    },[allUsers]);
+    },[]);
 
     useEffect(()=>{
         async function fetchUserData(){
@@ -51,12 +58,12 @@ export function HomePage(props : any){
             setBalance(response ?  response.data.balance : "null");
         }
         getBalance();
-    },[balance])
+    },[])
 
     return(
         <div className="h-[100vh] w-[100vw] bg-white">
             <div className="flex justify-between border border-slate-400 h-[10%] p-4 shadow-md">
-                <span><h1 className="text-2xl font-serif font-bold text-slate-600">DEx</h1></span>
+                <span><h1 className="text-2xl font-serif font-bold text-slate-600">PayDEx</h1></span>
                 <div className="flex justify-center items-center gap-2">
                     <p className="flex flex-row items-center justify-center">{userName ? userName.toLocaleUpperCase() : ""}</p>
                     <div className="h-10 w-10 rounded-full border border-black bg-slate-400  flex justify-center items-center">{userName ? userName.charAt(0).toUpperCase() : "?"}</div>
@@ -71,10 +78,10 @@ export function HomePage(props : any){
                 </div>
                 <div>
                 <h1 className="text-2xl m-2 mb-4 underline font-bold">Friends</h1>
-                    {allUsers.map(user=><div className="flex w-[100%] justify-between ">
+                    {isLoading?<Loading/> :allUsers.map(user=><div className="flex w-[100%] justify-between ">
                         <div className="flex justify-center items-center gap-2">
                         <div className="h-10 w-10 rounded-full border border-black bg-slate-400 flex justify-center items-center">{user?.username ? user?.username.charAt(0).toUpperCase() : "?"}</div>
-                        <h1>{user?.username}</h1>
+                        <h1>{user?.username }</h1>
                         </div>
                         <div className="mt-3"><Button title={"Send Money"} variant="Tertiary" onClick={()=>navigate("/send")}/></div>
                     </div>)}
